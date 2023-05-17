12ea1e29a629ef675aea785e30aa283988a3c53d
Aman kumar <amarraj8571841168@gmail.com>
2023-05-18 05:28:14 +0530

    day 11 complete

---
+++ dp11_striver.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+class memoriasiton{
+public:    
+  int minval(int r,int c,vector<vector<int>>trial,int n,vector<vector<int>> &dp){
+   if(dp[r][c]!=-1)  return dp[r][c];
+   if(r==n-1) return trial[r][c];
+
+int down=trial[r][c]+minval(r+1,c,trial,n,dp);
+int diagonal=trial[r][c]+minval(r+1,c+1,trial,n,dp);
+
+ return    dp[r][c]=min(down,diagonal);
+
+  }
+int minpath(vector<vector<int>> trial,int n){
+ 
+     vector<vector<int>> dp(n,vector<int>(n,-1));
+      return   minval(0,0,trial,n,dp);   
+   
+}
+
+};
+class tabulation{
+ public:   
+    int minpath(vector<vector<int>> trial ,int n){
+      vector<vector<int>>dp(n,vector<int>(n,0));
+      for(int i=0;i<n;i++){
+          dp[n-1][i]=trial[n-1][i];
+
+      }
+      for(int i=n-2;i>=0;i--){
+        for(int j=i;j>=0;j--){
+            int down=trial[i][j]+dp[i+1][j];
+            int diag=trial[i][j]+dp[i+1][j+1];
+            dp[i][j]=min(down,diag);
+
+        }
+      }
+        return dp[0][0];
+    }
+};
+int main(){
+   vector<vector<int>> triangle{{1},{2,3},{3,6,7},{8,9,6,10}};
+   int n=triangle.size();
+      tabulation obj;
+    cout<<"the min path sum is"<<obj.minpath(triangle,n);
+}
